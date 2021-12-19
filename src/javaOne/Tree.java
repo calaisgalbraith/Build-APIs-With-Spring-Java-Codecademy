9e147f378247cdfa1cade9fd0be1aef47c90306d
calaisgalbraith <calaisgalbraith@gmail.com>
2021-12-18 17:13:14 -0700

    finished Tree project

---
+++ src/javaOne/Tree.java
@@ -0,0 +1,19 @@
+package javaOne;
+
+public class Tree {
+
+    public static void main(String[] args) {
+        //Introduce self
+        System.out.println("Hello, I'm Calais and I'm learning to code in Java!'");
+
+        System.out.println("I'm going to plant a tree today!");
+
+        //print a tree
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+    }
+}
+
