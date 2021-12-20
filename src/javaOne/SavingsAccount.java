74927ce27f1d52d67df7ce425707742d69544d9b
calaisgalbraith <calaisgalbraith@gmail.com>
2021-12-19 19:51:49 -0700

    finished SavingsAccount project

---
+++ src/javaOne/SavingsAccount.java
@@ -0,0 +1,36 @@
+package javaOne;
+
+public class SavingsAccount {
+    int balance;
+
+    public SavingsAccount(int initialBalance){
+        balance = initialBalance;
+    }
+
+    //Check balance:
+    public void checkBalance(){
+        System.out.println("Hello!");
+        System.out.println("Your balance is " + balance);
+    }
+
+    //Deposit:
+    public void deposit(int amountToDeposit){
+        balance = balance + amountToDeposit;
+        System.out.println("You just deposited "+ amountToDeposit);
+    }
+
+    //Withdrawing:
+    public int withdraw(int amountToWithdraw){
+        balance = balance - amountToWithdraw;
+        System.out.println("You just withdrew "+ amountToWithdraw );
+        return amountToWithdraw;
+    }
+
+
+    public static void main(String[] args) {
+        SavingsAccount savings = new SavingsAccount(2000);
+        savings.checkBalance();
+        savings.deposit(100);
+        savings.withdraw(100);
+    }
+}
