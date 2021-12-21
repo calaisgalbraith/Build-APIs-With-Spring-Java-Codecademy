0ac7ed959f2719c3e683cefce58d9c780caa297c
calaisgalbraith <calaisgalbraith@gmail.com>
2021-12-20 18:35:48 -0700

    finished carloan project

---
+++ src/javaOne/CarLoan.java
@@ -0,0 +1,30 @@
+package javaOne;
+
+public class CarLoan {
+
+    public static void main(String[] args) {
+
+        int carLoan = 10000;
+        int loanLength = 3;
+        int interestRate = 5;
+        int downPayment = 2000;
+
+        //check if valid loan length
+        if(loanLength <= 0){
+            System.out.println("Error! You must take out a valid car loan");
+        }
+        else if(downPayment >= carLoan){ //check if can be paid in full
+            System.out.println("The car can be paid in full.");
+        }
+        else{
+            int remainingBalance = carLoan - downPayment;
+            int months = loanLength * 12;
+            int monthlyBalance = remainingBalance / months;
+            int interest = (monthlyBalance * interestRate) / 100;
+            int monthlyPayment = monthlyBalance + interest;
+
+            System.out.println(monthlyPayment);
+
+        }
+    }
+}
